e0bfb6a64804d048c888406ea2057ef065ab71f3
Markus Holtermann <info@markusholtermann.eu>
2013-10-05 16:41:48 +0200

    Add a grunt dev command ($ grunt dev) to watch for changes on .less and .js files as less.js does not reliable detect changes of included files

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,14 +53,46 @@ module.exports = function(grunt) {
         }
       }
     },
+    watch: {
+      less: {
+        files: [
+          '<%= pkg.name %>/static/less/*.less',
+          '<%= pkg.name %>/static/less/vendor/*.less'
+        ],
+        tasks: ['recess']
+      },
+      // js: {
+      //   files: [
+      //     '<%= jshint.all %>'
+      //   ],
+      //   tasks: ['jshint', 'uglify']
+      // }
+    },
+    clean: {
+      dist: [
+        'static/css/*.css',
+        'static/css/vendor/*.css',
+      ]
+    },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-recess');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'recess']);
+  grunt.registerTask('default', [
+    'clean',
+    'recess',
+    'uglify',
+  ]);
+
+  // Default task(s).
+  grunt.registerTask('dev', [
+    'watch',
+  ]);
 
   // Alias jshint to lint
   grunt.registerTask('lint', ['jshint']);
