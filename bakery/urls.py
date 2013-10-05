8caee39687f9f7132036d9d46dd3bad88f18e5f3
Markus Holtermann <info@markusholtermann.eu>
2013-10-05 16:43:10 +0200

    Only map /media/ through Django's static view in DEBUG mode

--- bakery/urls.py
+++ bakery/urls.py
@@ -20,9 +20,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.login_template = 'admin/custom_login.html'
